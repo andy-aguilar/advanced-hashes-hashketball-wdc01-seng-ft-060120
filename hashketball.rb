c85db2b28d4ee0c270e98354fd6e7f780963a301
Andres Aguilar <aaguil3@gmail.com>
2020-05-20 21:30:08 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -237,8 +237,8 @@ game_hash
       if info.class == Array
         if info[0].class == Hash
           info.each do |player, stats|
-            if player[shoe] > big_shoe 
-              big_shoe = player[shoe]
+            if player[:shoe] > big_shoe 
+              big_shoe = player[:shoe]
               big_shoe_baller = player[:player_name]
               binding.pry
             end
