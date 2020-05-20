0be481b3ffac2ee442186fc599b56e38f83a4e43
Andres Aguilar <aaguil3@gmail.com>
2020-05-20 21:06:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -186,7 +186,7 @@ def player_numbers (team_name)
   game_hash.each do |team, team_info|
     if team_info[:team_name] = team_name
       team_info[:players].each do |player_hash|
-        player_hash.each do |
+        player_hash.values_at 
         binding.pry
       end
     end
